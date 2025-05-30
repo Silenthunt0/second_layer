211c8fa3bbaf8aef67a72034b7c6fc1b4a1b87cf
VKFisher <vladkf@gmail.com>
2025-05-30 16:56:14 +0400

    tkinter env error handler, formatting

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup
 
-APP = ['main.py']
+APP = ["main.py"]
 OPTIONS = {
-    'argv_emulation': False,
-    'includes': ['tkinter'],
-    'iconfile': 'Icon.icns',
-    'packages': ['cryptography'],
-    'plist': {
-        'CFBundleName': 'Second Layer',
-        'CFBundleIdentifier': 'space.silenthunt.secondlayer',
-        'CFBundleVersion': '0.1.0',
-        'CFBundleShortVersionString': '0.1.0',
+    "argv_emulation": False,
+    "includes": ["tkinter"],
+    "iconfile": "Icon.icns",
+    "packages": ["cryptography"],
+    "plist": {
+        "CFBundleName": "Second Layer",
+        "CFBundleIdentifier": "space.silenthunt.secondlayer",
+        "CFBundleVersion": "0.1.0",
+        "CFBundleShortVersionString": "0.1.0",
     },
 }
 
 setup(
     app=APP,
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
 )
